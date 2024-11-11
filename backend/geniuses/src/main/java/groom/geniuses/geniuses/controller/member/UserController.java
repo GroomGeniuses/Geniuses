e662d147cf88a9aa8c09fe3bf08e79fdd1b59547
daehyuk1231 <155371052+daehyuk1231@users.noreply.github.com>
2024-11-11 10:54:26 +0900

    refactor: 코드 리팩토링 (#17)

--- backend/geniuses/src/main/java/groom/geniuses/geniuses/controller/member/UserController.java
+++ backend/geniuses/src/main/java/groom/geniuses/geniuses/controller/member/UserController.java
@@ -1,7 +1,7 @@
 package groom.geniuses.geniuses.controller.member;
 
 import groom.geniuses.geniuses.dto.member.UserDto;
-import groom.geniuses.geniusesm.service.member.UserService;
+import groom.geniuses.geniuses.service.member.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,29 +17,29 @@ public class UserController {
 
     // 사용자 정보 가져오기
     @GetMapping("/{userId}")
-    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
+    public ResponseEntity<UserDto> getUser(@PathVariable String userId) {
         UserDto user = userService.getUserById(userId);
         if (user != null) {
-            return ResponseEntity.ok(user); // 사용자 정보를 반환
+            return ResponseEntity.ok(user);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // 사용자 없음
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 
     // 사용자 정보 수정하기
     @PutMapping("/{userId}")
-    public ResponseEntity<Void> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
+    public ResponseEntity<Void> updateUser(@PathVariable String userId, @RequestBody UserDto userDto) {
         boolean updated = userService.updateUser(userId, userDto);
         if (updated) {
-            return ResponseEntity.ok().build(); // 수정 성공
+            return ResponseEntity.ok().build();
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 수정 실패
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     // 프로필 이미지 업로드
     @PostMapping("/{userId}/profile-image")
-    public ResponseEntity<String> uploadImage(@PathVariable Long userId, @RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadImage(@PathVariable String userId, @RequestParam("file") MultipartFile file) {
         boolean updated = userService.updateImage(userId, file);
         if (updated) {
             return ResponseEntity.ok("프로필 이미지 업데이트 성공");
